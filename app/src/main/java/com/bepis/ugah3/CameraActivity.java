79b2eb1db49f3cd58501503689f8835481928d95
nbennett30 <nbennett30@gatech.edu>
2018-02-10 19:39:06 -0500

    added image data grab + rgb conversion that doesnt crash

--- app/src/main/java/com/bepis/ugah3/CameraActivity.java
+++ app/src/main/java/com/bepis/ugah3/CameraActivity.java
@@ -7,6 +7,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.ImageFormat;
 import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.hardware.Camera;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
@@ -32,6 +34,8 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Toast;
+
+import org.opencv.core.Mat;
 
 import java.io.File;
 import java.io.IOException;
@@ -358,9 +362,13 @@ public class CameraActivity extends AppCompatActivity {
                 //Log.i("ImageReaderFrameWatcher", "Size: " + buffer.limit());
                 byte[] bytes = new byte[buffer.capacity()];
                 buffer.get(bytes);
-                Bitmap bitmapImage = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, null);
-                int center = bitmapImage.getPixel(bitmapImage.getWidth()/2, bitmapImage.getHeight()/2);
-
+                int format = image.getFormat();
+                /*Mat mat = ImageUtils.imageToMat(image);
+                double[] pix = mat.get(mat.height()/2, mat.width()/2);
+                double r = pix[2];
+                double g = pix[1];
+                double b = pix[0];*/
+                int[] rgb = getRGBIntFromPlanes(image.getPlanes());
                 image.close();
             }
         }
@@ -398,4 +406,63 @@ public class CameraActivity extends AppCompatActivity {
             previewSurface = null;
         }
     }
+
+    private int[] getRGBIntFromPlanes(Image.Plane[] planes) {
+        ByteBuffer yPlane = planes[0].getBuffer();
+        ByteBuffer uPlane = planes[1].getBuffer();
+        ByteBuffer vPlane = planes[2].getBuffer();
+
+        int bufferIndex = 0;
+        final int total = yPlane.capacity();
+        final int uvCapacity = uPlane.capacity();
+        final int width = planes[0].getRowStride();
+        int mHeight = surfaceView.getHeight();
+        int[] mRgbBuffer = new int[surfaceView.getWidth() * surfaceView.getHeight() * 3];
+        int yPos = 0;
+        for (int i = 0; i < mHeight; i++) {
+            int uvPos = (i >> 1) * width;
+
+            for (int j = 0; j < width; j++) {
+                if (uvPos >= uvCapacity-1)
+                    break;
+                if (yPos >= total)
+                    break;
+
+                final int y1 = yPlane.get(yPos++) & 0xff;
+
+            /*
+              The ordering of the u (Cb) and v (Cr) bytes inside the planes is a
+              bit strange. The _first_ byte of the u-plane and the _second_ byte
+              of the v-plane build the u/v pair and belong to the first two pixels
+              (y-bytes), thus usual YUV 420 behavior. What the Android devs did
+              here (IMHO): just copy the interleaved NV21 U/V data to two planes
+              but keep the offset of the interleaving.
+             */
+                final int u = (uPlane.get(uvPos) & 0xff) - 128;
+                final int v = (vPlane.get(uvPos+1) & 0xff) - 128;
+                if ((j & 1) == 1) {
+                    uvPos += 2;
+                }
+
+                // This is the integer variant to convert YCbCr to RGB, NTSC values.
+                // formulae found at
+                // https://software.intel.com/en-us/android/articles/trusted-tools-in-the-new-android-world-optimization-techniques-from-intel-sse-intrinsics-to
+                // and on StackOverflow etc.
+                final int y1192 = 1192 * y1;
+                int r = (y1192 + 1634 * v);
+                int g = (y1192 - 833 * v - 400 * u);
+                int b = (y1192 + 2066 * u);
+
+                r = (r < 0) ? 0 : ((r > 262143) ? 262143 : r);
+                g = (g < 0) ? 0 : ((g > 262143) ? 262143 : g);
+                b = (b < 0) ? 0 : ((b > 262143) ? 262143 : b);
+
+                mRgbBuffer[bufferIndex++] = ((r << 6) & 0xff0000) |
+                        ((g >> 2) & 0xff00) |
+                        ((b >> 10) & 0xff);
+            }
+        }
+        return mRgbBuffer;
+    }
+
 }
