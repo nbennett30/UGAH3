e4dd147ed8df8ae3fcefaabfddb0370423e6ac80
nbennett30 <nbennett30@gatech.edu>
2018-02-11 02:40:36 -0500

    overrode back button on win screen

--- app/src/main/java/com/bepis/ugah3/WinScreen.java
+++ app/src/main/java/com/bepis/ugah3/WinScreen.java
@@ -1,5 +1,6 @@
 package com.bepis.ugah3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class WinScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_win_screen);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
