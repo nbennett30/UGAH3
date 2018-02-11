779bfce8eb80ac69ff6b28a6d3e08ad39d78d559
tuturuam <aizadmoh@gmail.com>
2018-02-10 20:24:11 -0500

    added text view

--- app/src/main/java/com/bepis/ugah3/MainActivity.java
+++ app/src/main/java/com/bepis/ugah3/MainActivity.java
@@ -14,15 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         Button fab = (Button) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Bepis", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
                 Intent intent = new Intent(view.getContext(), CameraActivity.class);
                 startActivity(intent);
             }
