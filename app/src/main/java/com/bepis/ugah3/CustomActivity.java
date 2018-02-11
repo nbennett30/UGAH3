aa5fe3850a24f6e0f75faeb76dbabdf2542f8081
nbennett30 <nbennett30@gatech.edu>
2018-02-11 04:13:42 -0500

    edited manifest

--- app/src/main/java/com/bepis/ugah3/CustomActivity.java
+++ app/src/main/java/com/bepis/ugah3/CustomActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CustomActivity extends AppCompatActivity {
 
@@ -14,6 +16,13 @@ public class CustomActivity extends AppCompatActivity {
         setContentView(R.layout.activity_custom);
 
         Button custom = (Button) findViewById(R.id.button2);
+        EditText text = (EditText) findViewById(R.id.editText);
+        String color = text.getText().toString();
+        try {
+            Integer.parseInt(color, 16);
+        } catch (NumberFormatException e) {
+            Toast.makeText(getApplicationContext(), "Invalid Hex Code", Toast.LENGTH_SHORT).show();
+        }
         custom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
